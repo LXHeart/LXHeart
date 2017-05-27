55adcc56c1dedded45d9bb643d8d339dcb61d515
Heart <237263381@qq.com>
2017-05-27 16:29:19 +0800

    工具类

--- example/src/com/lxh/utils/Utils.java
+++ example/src/com/lxh/utils/Utils.java
@@ -39,18 +39,9 @@ public class Utils {
 		}
 		return flag;
 	}
-	/**
-	 * 将小数转换成百分制
-	 * @param num type：double double类型的数据
-	 * @return String
-	 */
-	public static String numToPercent(double num){
-		DecimalFormat df = new DecimalFormat("0.000%");
-		return df.format(num);
-	}
+	
 	
 	public static void main(String[] args) {
-		System.out.println(numToPercent(0.36548));
 	}
 
 }
